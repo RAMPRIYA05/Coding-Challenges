0a5194c3dcd33ffbf181126cb268ca26a0d42741
Ramp3565 <Ramp3565@CSL0709.global.chainsys.com>
2024-04-15 20:50:28 +0530

    print a,a1,a2

---
+++ CodingChallenges/src/com/chainsys/codingchallenges/Solution.java
@@ -0,0 +1,20 @@
+package com.chainsys.codingchallenges;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int a1= scan.nextInt();
+        int a2= scan.nextInt();
+        // Complete this line
+        // Complete this line
+        System.out.println(a);
+        System.out.println(a1);
+        System.out.println(a2);
+        // Complete this line
+        // Complete this line
+    }
+}
