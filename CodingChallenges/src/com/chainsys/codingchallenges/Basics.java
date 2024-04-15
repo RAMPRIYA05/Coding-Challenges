8501bfc1f2d4e2004545716f4e4c83830a9603cc
Ramp3565 <Ramp3565@CSL0709.global.chainsys.com>
2024-04-15 18:04:47 +0530

    Hello world

---
+++ CodingChallenges/src/com/chainsys/codingchallenges/Basics.java
@@ -0,0 +1,10 @@
+package com.chainsys.codingchallenges;
+
+public class Basics {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
