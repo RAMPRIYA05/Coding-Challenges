53cb1ad5dacdb1ac4bdda53210a6e0fcfbfd92b3
Ramp3565 <Ramp3565@CSL0709.global.chainsys.com>
2024-06-26 09:52:04 +0530

    Coding Challenges

---
+++ CodingChallenges/src/com/chainsys/codingchallenges/MultiplicationTable.java
@@ -0,0 +1,27 @@
+package com.chainsys.codingchallenges;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class MultiplicationTable {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        bufferedReader.close();
+        int i;
+        int result;
+        for(i=1;i<=10;i++)
+        {
+            result=N*i;
+        System.out.println(N+" x "+i+" = "+result);    
+        }
+    }
+}
